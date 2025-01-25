563b1e555ad1b2c15e56c5f47b5fa7ee9c7aa78b
itojun-1230 <juntou1230@gmail.com>
2025-01-25 16:14:12 +0900

    add: WebView

--- AIfriends/ContentView.swift
+++ AIfriends/ContentView.swift
@@ -1,33 +1,64 @@
-//
-//  ContentView.swift
-//  AIfriends
-//
-//  Created by 伊藤潤平 on 2025/01/19.
-//
-
 import SwiftUI
-import SwiftData
+import WebKit
+
+// WebView
+struct WebView: UIViewRepresentable {
+    
+    let url: URL
+    let webView: WKWebView
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let view = webView
+        view.allowsBackForwardNavigationGestures = true // スワイプジェスチャーを有効化
+        return view
+    }
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
+    
+    @State private var webView: WKWebView?
+    
     var body: some View {
         VStack {
-            // CreateFriendViewへ遷移する
-            NavigationStack {
-                Image(.title)
-                NavigationLink(destination: CreateFriendView()) {
-                    Text("友達を作りに行く！")
+            if let webView = webView {
+                VStack(spacing: 0){
+                    WebView(url: URL(string: "https://aifriends.deno.dev/")!, webView: webView) // ここに任意のURL
+                    Divider()
+                    // ここからナビゲーションボタン
+                    HStack {
+                        Button(action: {
+                            webView.goBack() // 戻る
+                        }, label: {
+                            Image(systemName: "chevron.backward")
+                        })
+                        Spacer()
+                            .frame(width: 40)
+                        Button(action: {
+                            webView.goForward() // 進む
+                        }, label: {
+                            Image(systemName: "chevron.forward")
+                        })
+                        Spacer()
+                    }
+                    .padding(.leading,40)
+                    .padding(.top,10)
+                    .padding(.bottom,7)
+                    .frame(maxWidth: .infinity)
+                    
                 }
-                .padding(100)
             }
-            
+        }
+        
+        .onAppear {
+            webView = WKWebView()
         }
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
